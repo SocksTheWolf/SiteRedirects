5e1ce68f049f096110c343603beb169c02037ccd
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-08-21 12:07:31 -0700

    throw together project
    super quick

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SiteRedirects
-A simple project to handle redirects to other URLS via CF KV entries
+
+A very, very simple project to handle redirects to other URLS via CF KV entries.
+
+This is to alleviate the 10 URL redirect rules limit on CF.
+
+## Setup
+
+Clone this repository, change the value of your KV id as well as the variable value of `FALLBACK_URL`.
+Push your changes to CF.
+
+## To Use
+
+Your KV keys are the pathnames to match. These will be checked against in lowercase format.
+The values will be the destination.
---
+++ worker.js
@@ -0,0 +1,17 @@
+export default {
+  async fetch(request, env, ctx) {
+    const {pathname} = new URL(request.url);
+    const urlPath = pathname.split("/", 2).pop().toLowerCase();
+    var location = null;
+
+    if (urlPath !== "") {
+      // raw lookup in the KV db for the location to go to.
+      // if this fails, location = null
+      location = await env.REDIRECTS.get(urlPath);
+    }
+    // write the fallback url if location is not null
+    location ??= env.FALLBACK_URL;
+
+    return Response.redirect(location, 302);
+  },
+};
